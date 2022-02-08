a6f4843b47f96a48c13c598c7bc363e90ccbb104
Gayane25 <gayabennu@gmail.com>
2022-02-08 22:49:11 +0400

    feat:added divs and img tags for coat and flag

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 const container = document.querySelector('#root');
+const wrapperInp = document.querySelector('#input-wrapper');
 const input = document.querySelector('.searchInp');
 const searchButton = document.querySelector('.searchBtn');
 const countryName = document.querySelector('.countryName');
+const countryCapital = document.querySelector('.countryCapital');
+const countryPopulation = document.querySelector('.countryPopulation');
+let countryFlag = document.querySelector('.countryFlag');
+let countryCoat = document.querySelector('.countryCoat');
+let imgFlag = document.querySelector('.imgFlag');
+let imgCoat = document.querySelector('.imgCoat');
+// let countryMap = document.querySelector('.countryMap');
 
 fetch('https://restcountries.com/v3.1/all')
   .then(response => response.json())
   .then(data => getData(data));
 
 function getData(data) {
-  let countries = data.map(item => item.name.common);
+  // let countries = data.map(item => item.name.common);
   searchButton.addEventListener('click', function () {
-    for (let country of countries) {
-      if (input.value === country) {
-        countryName.textContent = `Welcome to ${country}`;
+    for (let country of data) {
+      if (input.value === country.name.common) {
+        wrapperInp.style.paddingTop = '50px';
+        wrapperInp.style.paddingBottom = '50px';
+
+        countryName.textContent = `${country.name.common}, oficially ${country.name.official}`;
+        countryCapital.textContent = `The capital is ${country.capital}`;
+        countryPopulation.textContent = `Its population is ${country.population}`;
+        imgFlag.src = country.flags.png;
+        imgFlag.style.width = '500px';
+        imgFlag.style.height = '350px';
+        imgCoat.src = country.coatOfArms.png;
+        imgCoat.style.width = '500px';
+        imgCoat.style.height = '350px';
       }
     }
   });
